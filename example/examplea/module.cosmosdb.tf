3061797f63a9896b55ffaea20d9aa1e32fe3bf51
James Woolfenden <james.woolfenden@gmail.com>
2023-08-31 13:17:26 +0100

    add resource group

--- example/examplea/module.cosmosdb.tf
+++ example/examplea/module.cosmosdb.tf
@@ -1,10 +1,16 @@
 module "cosmosdb" {
   source            = "../../"
   common_tags       = var.common_tags
-  resource_group    = "examplea"
-  location          = "UK South"
+  resource_group    = azurerm_resource_group.examplea.name
+  location          = azurerm_resource_group.examplea.location
   account_name      = "secondaccountjgw"
   failover_location = var.failover_location
   key_vault_key_id  = "https://examplekeyvaultjgw.vault.azure.net/keys/temp"
   kind              = "GlobalDocumentDB"
+}
+
+
+resource "azurerm_resource_group" "examplea" {
+  name     = "examplea"
+  location = "UK South"
 }
